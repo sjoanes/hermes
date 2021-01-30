27144e9da61e6c7a0352882804247ea0b3929dcc
Savio Joanes <savio.joanes@tophatmonocle.com>
2021-01-30 17:45:08 -0500

    feat add persistence

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField()
+
+
+class Game(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    current_poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    round = models.IntegerField(default=1)
+    points = models.IntegerField(default=0)
+    attempts = models.IntegerField(default=3)
+
+
+class PollResponse(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    response = models.TextField()
+
+    
